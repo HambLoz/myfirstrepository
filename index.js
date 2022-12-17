3f110f03eea2dbb402ea6a7457dfa5988d4f6757
HambLoz <hambart.lozano@gmail.com>
2022-12-16 21:25:41 -0500

    se agrego vercel

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/',  (req, res) => {
+    res.send('Hola mundo🗺️')
+})
+
+app.listen(8000, () => {
+    console.log("El servidor está corriendo en http://localhost:8000")
+} ) 
